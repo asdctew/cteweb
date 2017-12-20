d2af627a6a77fb6c4353cb47c43577c8649a6e01
Parker Nilson <nilsopar@gmail.com>
2017-12-20 14:05:16 -0700

    added css and js folders, along with general style.css and started ease_scroller.js (a function for cubic bezier controlled focus point scrolling down the page -- Ask Parker if you have questions)

---
+++ js/ease_scroller.js
@@ -0,0 +1,11 @@
+$(function(){
+   console.log("This is the smooth scroller! by, Parker! Nilson :o)");
+
+   var scrollContainers = $('.scroll_container');
+
+   for(var i = 0; i < scrollContainers.length; ++i){
+      //format scroll containers positions
+      
+   }
+
+});
